de918b85ac4f388efac35fc6300f5177fa654271
Oksana-Pak <okspak7@gmail.com>
2023-02-25 17:23:53 +0200

    add task

---
+++ contacts.js
@@ -0,0 +1,63 @@
+const fs = require('fs').promises;
+const path = require('path');
+const crypto = require('crypto');
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath, 'utf-8');
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(`Something went wrong. ${error.message}`);
+  }
+};
+
+const getContactById = async contactId => {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(({ id }) => String(id) === String(contactId));
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  } catch (error) {
+    console.log(`Something went wrong. ${error.message}`);
+  }
+};
+
+const removeContact = async contactId => {
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(({ id }) => String(id) === String(contactId));
+    if (idx === -1) {
+      return null;
+    }
+    const contactToRemove = contacts.splice(idx, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contactToRemove;
+  } catch (error) {
+    console.log(`Something went wrong. ${error.message}`);
+  }
+  //   try {
+  //     const contacts = await listContacts();
+  //     const newContactsList = contacts.filter(({ id }) => String(id) !== String(contactId));
+  //     await fs.writeFile(contactsPath, JSON.stringify(newContactsList, null, 4));
+  //   } catch (error) {
+  //     console.log(`Something went wrong. ${error.message}`);
+  //   }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = { name, email, phone, id: crypto.randomUUID() };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 4));
+    return newContact;
+  } catch (error) {
+    console.log(`Something went wrong. ${error.message}`);
+  }
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
---
+++ index.js
@@ -0,0 +1,42 @@
+const { program } = require('commander');
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone')
+  .parse(process.argv);
+
+const argv = program.opts();
+
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contactsAll = await listContacts();
+      console.table(contactsAll);
+      break;
+
+    case 'get':
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+
+    case 'add':
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case 'remove':
+      const contactToRemove = await removeContact(id);
+      console.log(contactToRemove);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
